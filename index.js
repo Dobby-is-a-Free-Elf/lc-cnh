3711401342619b804450e434a5af85baed4293b8
Laise Xavier <laise18x@gmail.com>
2022-03-12 16:44:40 -0300

    Criação da classe Driver e realização do TDD

---
+++ index.js
@@ -0,0 +1,4 @@
+// import Driver from 'src/models/Driver'
+const Driver = require('src/models/Driver')
+
+const driver = new Driver()
---
+++ src/models/Driver.js
@@ -0,0 +1,11 @@
+module.exports = class Driver {
+  name = ''
+  id = 0
+
+  constructor(name, id) {
+    this.name = name
+    this.id = id
+  }
+}
+
+// export default Driver
