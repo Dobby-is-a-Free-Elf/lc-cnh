29b68d02251781c19e33981a48829b430c893add
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2022-03-13 17:22:16 -0300

    Fazendo configuração básica do webpack seguindo o tutorial oficial para a versão 5

--- index.js
+++
@@ -1,8 +0,0 @@
-// import Driver from 'src/models/Driver'
-const Driver = require("src/models/Driver");
-
-const id = 123234
-const name = 'dsfssfd'
-const license = new License(true, asdasd,'A', 0)
-
-const driver = new Driver(id, name, license);
